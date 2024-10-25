30dd91a2164575aabe6bc4dd6fbe5b0b947925e1
Sorogo7 <sarikisorogo@gmail.com>
2024-10-25 14:10:03 +0000

    first commit

---
+++ operators.py
@@ -0,0 +1,113 @@
+import random
+# first_number = int(input("enter first number: "))
+# second_number = int(input("enter second first_number: "))
+# if(first_number > second_number):
+#     print(second_number , first_number)
+# else:
+#      print(first_number , second_number)
+
+
+
+
+
+# number = int(input("enter number less than 30: "))
+# if(number>=30):
+#     print("too high")
+# else:
+#     print("thank you")
+
+
+
+# color= input("enter favarite color: ")
+# if color== "red" or color=="RED" or color== "Red":
+#     print("i like red too")
+# else:
+#     print("i dont like ", color)
+
+
+
+# color= input("enter favarite color: ")
+# if color=="red" or color=="RED" or color =="Red":
+#     print ("i like red too")
+# else:
+#     print("i dont like" , color)
+
+# ask the user if it is raining
+# if the answer is "yes", ask if it is windy
+# if the answer is yes to the second question
+#print("enter[yes] or [no] ")
+
+# user_input1= int(input("enter your age? "))
+# if  user_input1 >=18 :
+#     print("you can vote")
+# elif user_input1==17:
+#     print("i cant drive")
+# elif user_input1 == 16:
+#     print("you can learn to drive")
+# else:
+#     print("you can go trick or treating")
+
+##  take the input and record attendance
+ ## generate seq of number between 1 and 10
+# for each_number in range("1, 10"):
+#     print("Each Number: => ", each_number)
+# for each_number in range("1, 11"):
+#     print("Enter Number: => ", each_number)
+
+# generate the seq of even number b/n 1 and 10 (inclusive)
+# for 
+    
+# 
+#ask user to enter their name and print it out three times
+# user_names= input("enter user name: ")
+# number= int(input("enter number of times: "))
+# for each_name in range(number):
+#     print("your name is: " , user_names)
+
+
+#0041
+# user_name= input("enter your name: ")
+# number_times= int(input("enter number of times: "))
+# if number_times<10:
+#     for each_name in range(1,number_times+1):
+#         print("your name is ", user_name)
+# else:
+#     for each_item in range(1,4):
+#         print("too high")
+
+# print(" a class attendace register")
+# attendance = input("have you registed or no ?")
+# while (attendance == "no"):
+#     username = input("enter your name to register: ")
+#     print("confirm name: ", username)
+
+#46  
+# user_number= int(input("enter a number: "))
+# while (user_input<= 5):
+#     print(user_input)
+#     user_input=int(input("enter the next number"))
+
+#     print("the number you have entered is ", user_input)
+
+#..............random class....................
+
+# pick_item= random.choice(["red", "green", "blue"])
+# print(pick_item)
+
+# rand_num= random.randint(0,20)
+# print(rand_num)
+
+# num=random.random
+# # print(num)
+
+### day 5
+# user_input = int(input("enter a number b/n 10 and 20: "))
+# while((user_input<10) or (user_input>20)):
+#     if(user_input < 10):
+#         print("too low")
+# #user_input=int(input("enter guess again: "))
+#     if(user_input > 20):
+#         print("too high")
+#     ###user_input=int(input("enter guess anaim: "))
+#     user_input=int(input("enter guess again"))
+# print("thank you")
---
+++ variable.py
@@ -0,0 +1,12 @@
+# type casting
+#num1 = input("enter a first number: ")
+#num2 = input("enter second number: ")
+#result= int(num1) + int(num2)
+#print("the sum of the two numbers is " , result)
+# type casting 2
+# take three input and convert them to an integer
+
+first_number = int(input("enter your first number: "))
+second_number = int(input("enter the second number: "))
+
+##.................... conditional or control statements................
